f79083f4fd900ec1098dd19b15ff624a82610d70
Priyank Chheda <p.chheda29@gmail.com>
2018-12-04 16:35:14 +0530

    add account verify credentials get api

--- account.go
+++ account.go
@@ -25,3 +25,25 @@ func GetAccountSettings(client *http.Client) (string, error) {
 
 	return string(bits), nil
 }
+
+// GetAccountVerifyCredentials represents of the requesting user if authentication was successful;
+// else return an error message
+func GetAccountVerifyCredentials(client *http.Client) (string, error) {
+	response, err := client.Get("https://api.twitter.com/1.1/account/verify_credentials.json")
+	if err != nil {
+		return "", err
+	}
+	defer response.Body.Close()
+
+	bits, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		return "", err
+	}
+
+	err = CheckForResponseError(bits)
+	if err != nil {
+		return "", err
+	}
+
+	return string(bits), nil
+}
